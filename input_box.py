a1ae6064f5882f3111fbfd5cdcd02a7baef57bbf
Moonnight1027 <cody.guangchen@gmail.com>
2024-06-29 00:59:14 +0800

    update

--- input_box.py
+++ input_box.py
@@ -3,70 +3,86 @@ import pyperclip
 import sys
 import os
 from io import StringIO
+import random  # 导入 random 模块
 
 FPS = 60
 WIDTH = 800
-HIGH = 600
+HEIGHT = 600
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-YELLOW = (255, 255, 0)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HIGH))
-clock  = pygame.time.Clock()
-running  = True
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+running = True
 
-font = os.path.join("Yozai-Medium.ttf")
+font_path = os.path.join("font/Yozai-Medium.ttf")
 
 
-class InputBox(pygame.sprite.Sprite,):
+class InputBox(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.box_width = WIDTH-100   #輸入框大小
-        self.box_height = HIGH-100
-        self.cursor_x = 20            #游標座標
+        self.box_width = WIDTH - 100
+        self.box_height = HEIGHT - 100
+        self.cursor_x = 20
         self.cursor_y = 20
         self.image = pygame.Surface((self.box_width, self.box_height))
         self.image.fill((20, 20, 20))
         self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH/2
+        self.rect.centerx = WIDTH / 2
         self.rect.top = 50
-        self.problem={
-            "problem" : "Add two numbers",
-            "input": ["1\n2", "3\n4"],
-            "expected_output": ["3\n", "7\n"]
-        }
 
-        self.active = False #是否選中輸入框
-        self.input_set = set() #現在選中的鍵值
-        self.input_interval = {} #選中的鍵值冷卻時間
-        self.text = f"\"\"\"\r\n{self.problem['problem']}\r\n\r\n\"\"\"" #文本
+        self.active = False
+        self.input_set = set()
+        self.input_interval = {}
+        self.font = pygame.font.Font(font_path, 28)
 
-        self.font = pygame.font.SysFont("Yozai-Medium.ttf", 30)
-    
+        # 定义多个题目
+        self.problems = [
+            {
+                "problem": "Add two numbers",
+                "input": ["1\n2", "3\n4"],
+                "expected_output": ["3\n", "7\n"]
+            },
+            {
+                "problem": "Multiply two numbers",
+                "input": ["2\n3", "4\n5"],
+                "expected_output": ["6\n", "20\n"]
+            },
+            {
+                "problem": "Subtract two numbers",
+                "input": ["5\n3", "10\n4"],
+                "expected_output": ["2\n", "6\n"]
+            }
+        ]
+
+        self.set_random_problem()
+
+    def set_random_problem(self):
+        self.problem = random.choice(self.problems)
+        self.text = f"{self.problem['problem']}\r\n-------------------------\r\nType the code below ( Press Enter to submit ):\r\n"
+
     def update(self):
         self.image.fill((20, 20, 20))
-        
+
         if self.active:
             current_time = pygame.time.get_ticks()
             self.text_x = 20
             self.text_y = 20
             for i in self.text.split("\r\n"):
-                self.text_surface = self.font.render(i, True,(200, 200, 200), (20, 20, 20))
+                self.text_surface = self.font.render(i, True, (200, 200, 200), (20, 20, 20))
                 self.image.blit(self.text_surface, (self.text_x, self.text_y))
-                self.text_y += 20
+                self.text_y += 32
 
-            self.cursor_x = self.text_surface.get_width()+23
-            self.cursor_y = self.text_y - 20
+            self.cursor_x = self.text_surface.get_width() + 23
+            self.cursor_y = self.text_y - 30
 
-            pygame.draw.rect(self.image, (200, 200, 200), [10, 10, self.box_width-20, self.box_height-20], 2) #灰色邊框
-            if(current_time%1000 > 500):   #游標閃爍
-                pygame.draw.line(self.image, (200, 200, 200), (self.cursor_x, self.cursor_y), (self.cursor_x, self.cursor_y+15), 2)
-            
+            pygame.draw.rect(self.image, (200, 200, 200), [10, 10, self.box_width - 20, self.box_height - 20], 2)  # 灰色边框
+            if (current_time % 1000 > 500):  # 光标闪烁
+                pygame.draw.line(self.image, (200, 200, 200), (self.cursor_x, self.cursor_y),
+                                 (self.cursor_x, self.cursor_y + 30), 2)
+
             for i in self.input_set:
-                if(current_time-self.input_interval[i] > 155):
+                if current_time - self.input_interval[i] > 155:
                     self.input_interval[i] = current_time
                     if i == '\b':
                         self.text = self.text[:-1]
@@ -74,9 +90,9 @@ class InputBox(pygame.sprite.Sprite,):
                         self.text += i
 
         else:
-            pygame.draw.rect(self.image, (140, 140, 140), [10, 10, self.box_width-20, self.box_height-20], 2) #灰色邊框
-            pygame.draw.line(self.image, (20, 20, 20), (self.cursor_x, self.cursor_y), (self.cursor_x, self.cursor_y+10), 2)
-
+            pygame.draw.rect(self.image, (140, 140, 140), [10, 10, self.box_width - 20, self.box_height - 20], 2)  # 灰色边框
+            pygame.draw.line(self.image, (20, 20, 20), (self.cursor_x, self.cursor_y),
+                             (self.cursor_x, self.cursor_y + 30), 2)
 
     def execute_code(self, user_code, problem):
         results = []
@@ -85,26 +101,35 @@ class InputBox(pygame.sprite.Sprite,):
             sys.stdin = StringIO(problem["input"][i])
             output = StringIO()
             sys.stdout = output
-            exec(user_code, {}, {})
-            if output.getvalue() == problem["expected_output"][i]:
-                results.append(f"Test case {i+1}: Passed")
-            else:
-                results.append(f"Test case {i+1}: Failed - Expected {problem['expected_output'][i].split()}, but got {output.getvalue().split()}")
+            try:
+                exec(user_code, {}, {})
+            except Exception as e:
+                results.append(f"Test case {i + 1}: Failed，Error {e}")
                 ap = False
-            sys.stdout = sys.__stdout__
-        return (results, ap)
+                break
+            finally:
+                sys.stdin = sys.__stdin__
+                sys.stdout = sys.__stdout__
 
-    def event_process(self, event): #處理事件
+            if output.getvalue() == problem["expected_output"][i]:
+                results.append(f"Test case {i + 1}: Passed")
+            else:
+                results.append(
+                    f"Test case {i + 1}: Failed - Expected {problem['expected_output'][i].strip()}，but got {output.getvalue().strip()}")
+                ap = False
+
+        return results, ap
+
+    def event_process(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             pass
         if event.type == pygame.MOUSEBUTTONUP:
-            pass            
+            pass
         if event.type == pygame.KEYDOWN:
             ctrl_pressed = pygame.key.get_mods() & pygame.KMOD_CTRL
             if ctrl_pressed and event.key == pygame.K_v:
-                # 检测到 Ctrl+V 组合键
                 self.text += pyperclip.paste()
-                
+
             elif event.key == pygame.K_BACKSPACE:
                 self.input_set.add('\b')
                 self.input_interval['\b'] = -100
@@ -116,7 +141,7 @@ class InputBox(pygame.sprite.Sprite,):
                 self.input_interval['    '] = -100
             elif event.key == pygame.K_KP_ENTER:
                 results, ap = self.execute_code(self.text, self.problem)
-                
+
                 with open('result.txt', 'a') as f:
                     f.write(self.text)
                     f.write(str(ap))
@@ -142,40 +167,31 @@ class InputBox(pygame.sprite.Sprite,):
             else:
                 self.input_set.discard(event.unicode)
 
-    
     def inputbox_print(self, s):
         self.text += s
-
 
 
 all_sprites = pygame.sprite.Group()
 input_box = InputBox()
 all_sprites.add(input_box)
 
-#遊戲迴圈
 while running:
     clock.tick(FPS)
-    #取得輸入
-    for event in pygame.event.get(): #event 會是個列表
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if input_box.active:
             input_box.event_process(event)
         if event.type == pygame.MOUSEBUTTONDOWN:
             input_box.active = True if input_box.rect.collidepoint(event.pos) else False
-                
-    
-            
-    #更新遊戲
-    all_sprites.update()
 
-    #畫面顯示
-    pygame.display.update() #更新畫面
+    all_sprites.update()
     screen.fill(WHITE)
     all_sprites.draw(screen)
-
+    pygame.display.update()
 
 pygame.quit()
+
 
 
 """
