1a2ed3f735c1cbc099df46c843fee41b1d3befb5
Moonnight1027 <cody.guangchen@gmail.com>
2024-07-02 16:40:07 +0800

    update

--- input_box.py
+++ input_box.py
@@ -5,7 +5,6 @@ import os
 from io import StringIO
 import random
 
-
 FPS = 60
 WIDTH = 1000
 HEIGHT = 750
@@ -16,17 +15,13 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
-
 font_path = os.path.join("font/Yozai-Medium.ttf")
-
 
 class InputBox(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.box_width = WIDTH - 100
         self.box_height = HEIGHT - 100
-        self.cursor_x = 20
-        self.cursor_y = 20
         self.image = pygame.Surface((self.box_width, self.box_height))
         self.image.fill((20, 20, 20))
         self.rect = self.image.get_rect()
@@ -34,12 +29,15 @@ class InputBox(pygame.sprite.Sprite):
         self.rect.top = 50
 
         self.active = False
-        self.input_set = set()
-        self.input_interval = {}
         self.font = pygame.font.Font(font_path, 28)
         self.all_passed = False
 
-        # 定義題庫
+        self.cursor_pos = 0
+        self.text = ""
+        self.input_lines = [""]
+        self.cursor_line = 0
+        self.cursor_column = 0
+
         self.problems = [
             {
                 "problem": "Add two numbers",
@@ -92,43 +90,34 @@ class InputBox(pygame.sprite.Sprite):
 
     def set_random_problem(self):
         self.problem = random.choice(self.problems)
-        self.text = f"{self.problem['problem']}\r\n-------------------------\r\nType the code below ( Press Enter to submit ):\r\n"
+        self.text = f"{self.problem['problem']}\n-------------------------\nType the code below ( Press Enter to submit ):\n"
+        self.input_lines = self.text.split("\n")
+        self.cursor_line = len(self.input_lines) - 1
+        self.cursor_column = len(self.input_lines[self.cursor_line])
 
     def update(self):
         self.image.fill((20, 20, 20))
-
         if self.active:
             current_time = pygame.time.get_ticks()
             self.text_x = 20
             self.text_y = 20
-            for i in self.text.split("\r\n"):
-                self.text_surface = self.font.render(i, True, (200, 200, 200), (20, 20, 20))
-                self.image.blit(self.text_surface, (self.text_x, self.text_y))
+            for i, line in enumerate(self.input_lines):
+                text_surface = self.font.render(line, True, (200, 200, 200), (20, 20, 20))
+                self.image.blit(text_surface, (self.text_x, self.text_y))
                 self.text_y += 32
 
-            self.cursor_x = self.text_surface.get_width() + 23
-            self.cursor_y = self.text_y - 30
+            if (current_time % 1000 > 500):
+                cursor_surface = self.font.render("|", True, (200, 200, 200), (20, 20, 20))
+                cursor_x = 20 + self.font.size(self.input_lines[self.cursor_line][:self.cursor_column])[0]
+                cursor_y = 20 + self.cursor_line * 32
+                self.image.blit(cursor_surface, (cursor_x, cursor_y))
 
-            pygame.draw.rect(self.image, (200, 200, 200), [10, 10, self.box_width - 20, self.box_height - 20], 2)  # 灰色边框
-            if (current_time % 1000 > 500):  # 光標閃爍
-                pygame.draw.line(self.image, (200, 200, 200), (self.cursor_x, self.cursor_y),
-                                 (self.cursor_x, self.cursor_y + 30), 2)
-
-            for i in self.input_set:
-                if current_time - self.input_interval[i] > 155:
-                    self.input_interval[i] = current_time
-                    if i == '\b':
-                        self.text = self.text[:-1]
-                    else:
-                        self.text += i
-
+            pygame.draw.rect(self.image, (200, 200, 200), [10, 10, self.box_width - 20, self.box_height - 20], 2)
         else:
-            pygame.draw.rect(self.image, (140, 140, 140), [10, 10, self.box_width - 20, self.box_height - 20], 2)  # 灰色边框
-            pygame.draw.line(self.image, (20, 20, 20), (self.cursor_x, self.cursor_y),
-                             (self.cursor_x, self.cursor_y + 30), 2)
+            pygame.draw.rect(self.image, (140, 140, 140), [10, 10, self.box_width - 20, self.box_height - 20], 2)
 
     def execute_code(self, user_code, problem):
-        user_code = user_code.split("Type the code below ( Press Enter to submit ):\r\n")[-1]
+        user_code = user_code.split("Type the code below ( Press Enter to submit ):\n")[-1]
         results = []
         ap = True
         for i in range(len(problem["input"])):
@@ -162,52 +151,85 @@ class InputBox(pygame.sprite.Sprite):
         if event.type == pygame.KEYDOWN:
             ctrl_pressed = pygame.key.get_mods() & pygame.KMOD_CTRL
             if ctrl_pressed and event.key == pygame.K_v:
-                self.text += pyperclip.paste()
-
+                paste_text = pyperclip.paste()
+                self.input_lines[self.cursor_line] = (
+                    self.input_lines[self.cursor_line][:self.cursor_column] +
+                    paste_text +
+                    self.input_lines[self.cursor_line][self.cursor_column:]
+                )
+                self.cursor_column += len(paste_text)
             elif event.key == pygame.K_BACKSPACE:
-                self.input_set.add('\b')
-                self.input_interval['\b'] = -100
+                if self.cursor_column > 0:
+                    self.input_lines[self.cursor_line] = (
+                        self.input_lines[self.cursor_line][:self.cursor_column - 1] +
+                        self.input_lines[self.cursor_line][self.cursor_column:]
+                    )
+                    self.cursor_column -= 1
+                elif self.cursor_line > 0:
+                    self.cursor_column = len(self.input_lines[self.cursor_line - 1])
+                    self.input_lines[self.cursor_line - 1] += self.input_lines.pop(self.cursor_line)
+                    self.cursor_line -= 1
             elif event.key == pygame.K_RETURN:
-                self.input_set.add('\r\n')
-                self.input_interval['\r\n'] = -100
+                self.input_lines.insert(self.cursor_line + 1, self.input_lines[self.cursor_line][self.cursor_column:])
+                self.input_lines[self.cursor_line] = self.input_lines[self.cursor_line][:self.cursor_column]
+                self.cursor_line += 1
+                self.cursor_column = 0
             elif event.key == pygame.K_TAB:
-                self.input_set.add('    ')
-                self.input_interval['    '] = -100
+                self.input_lines[self.cursor_line] = (
+                    self.input_lines[self.cursor_line][:self.cursor_column] +
+                    '    ' +
+                    self.input_lines[self.cursor_line][self.cursor_column:]
+                )
+                self.cursor_column += 4
+            elif event.key == pygame.K_LEFT:
+                if self.cursor_column > 0:
+                    self.cursor_column -= 1
+                elif self.cursor_line > 0:
+                    self.cursor_line -= 1
+                    self.cursor_column = len(self.input_lines[self.cursor_line])
+            elif event.key == pygame.K_RIGHT:
+                if self.cursor_column < len(self.input_lines[self.cursor_line]):
+                    self.cursor_column += 1
+                elif self.cursor_line < len(self.input_lines) - 1:
+                    self.cursor_line += 1
+                    self.cursor_column = 0
+            elif event.key == pygame.K_UP:
+                if self.cursor_line > 0:
+                    self.cursor_line -= 1
+                    self.cursor_column = min(self.cursor_column, len(self.input_lines[self.cursor_line]))
+            elif event.key == pygame.K_DOWN:
+                if self.cursor_line < len(self.input_lines) - 1:
+                    self.cursor_line += 1
+                    self.cursor_column = min(self.cursor_column, len(self.input_lines[self.cursor_line]))
             elif event.key == pygame.K_KP_ENTER:
                 if self.all_passed:
                     pygame.quit()
                     sys.exit()
-                results, ap = self.execute_code(self.text, self.problem)
+                results, ap = self.execute_code('\n'.join(self.input_lines), self.problem)
                 self.all_passed = ap
 
                 with open('result.txt', 'a') as f:
-                    f.write(self.text)
+                    f.write('\n'.join(self.input_lines))
                     f.write(str(ap))
                     for line in results:
                         f.write(line)
-                self.text += '\r\n'
+                self.text += '\n'
                 for i in results:
-                    self.text += '\r\n'
+                    self.text += '\n'
                     self.text += i
                 return ap
             elif event.key == pygame.K_ESCAPE:
                 self.kill()
             else:
-                self.input_set.add(event.unicode)
-                self.input_interval[event.unicode] = -100
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_BACKSPACE:
-                self.input_set.remove('\b')
-            elif event.key == pygame.K_RETURN:
-                self.input_set.remove('\r\n')
-            elif event.key == pygame.K_TAB:
-                self.input_set.remove('    ')
-            else:
-                self.input_set.discard(event.unicode)
+                self.input_lines[self.cursor_line] = (
+                    self.input_lines[self.cursor_line][:self.cursor_column] +
+                    event.unicode +
+                    self.input_lines[self.cursor_line][self.cursor_column:]
+                )
+                self.cursor_column += 1
 
     def inputbox_print(self, s):
         self.text += s
-
 
 all_sprites = pygame.sprite.Group()
 input_box = InputBox()
@@ -229,4 +251,5 @@ while running:
     pygame.display.update()
 
 pygame.quit()
+
 
